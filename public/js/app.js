fccf2331142a9ed36b80f625056a7a28353ecf2f
arielnasimba <arielkafe@hotmail.com>
2024-01-20 15:28:17 +0100

    end project

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,35 @@
 let counters = document.querySelectorAll(".counter");
-console.log(counters);
 
-function increase_cpt(nb_max, counter) {
-    
+function counter_increase(nb_max, cpt) {
+    let cpt_increment = () => {
+        let nb_to_increase = +cpt.innerText;
+        let incrementator = Math.ceil(nb_max / 200); // More we divide the value by something higher = more slowly will be the increment and math.ceil output : smth entier like 5 , 8 never 5.2 or 6.32
 
-    while ( parseInt(counter.innerText ) < nb_max) {
-        parseInt(counter.innerText) += 50;
+        if (nb_to_increase < nb_max) {
+            let new_value = Math.min(nb_max, Math.ceil(nb_to_increase + incrementator));
+            cpt.innerText = `${new_value}`
+            // cpt.innerText = `${Math.ceil(nb_to_increase + incrementator)}`;
+            setTimeout(cpt_increment, 1); // recurrence with setTimeOut
+        }
     }
+    cpt_increment(); // recurrence of cpt_increment
 }
-
-const content =  counters[0].innerText;
-console.log(content);
-let t = counters[0].innerText;
-console.log(typeof parseInt (t) );
-
-// element.innerHTML = htmlString
-
-console.log(counters[0].innerText+10);
 
 window.addEventListener("load", (event) => {
     console.log("page is fully loaded");
 
+    for (let index = 0; index < counters.length; index++) {
+
+        counters[index].innerText = '0';
+        
+        if (index == 0) {
+            counter_increase(12000, counters[index])
+        }
+        else if (index == 1) {
+            counter_increase(5000, counters[index])
+        }
+        else if (index == counters.length -1) {
+            counter_increase(7500, counters[index])
+        }
+    }
   });
